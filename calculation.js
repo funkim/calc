c8cb776effbd65ad335066f854d83e1af9bb8214
funkim <mcking172@skiff.com>
2024-02-16 21:07:26 -0500

    base calculator ui done

--- calculation.js
+++ calculation.js
@@ -1,8 +1,8 @@
 let numberOperator = [];
 let operator = ["+", "-", "/","*"]
-let number = ["0","1","2","3","4","5","6","7","8","9"]
+let number = ["0","1","2","3","4","5","6","7","8","9"];
 
 function showResult() {
-    const correctEquation = parseInt(numberOperator)
+    const correctEquation = parseInt(numberOperator);
     return parseFloat
 }
