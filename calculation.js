2fca9912f018a4865b5cc5a74e917d11a863252d
funkim <mcking172@skiff.com>
2024-02-17 18:04:33 -0500

    Started laying out + creating the buttons.

--- calculation.js
+++ calculation.js
@@ -1,8 +1,9 @@
-let numberOperator = [];
-let operator = ["+", "-", "/","*"]
-let number = ["0","1","2","3","4","5","6","7","8","9"];
-
 function showResult() {
     const correctEquation = parseInt(numberOperator);
     return parseFloat
 }
+
+const calculator = document.querySelector(".calc")
+
+
+
