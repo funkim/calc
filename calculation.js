1196cdcb63366cfbd62dd185aa66c698fd428fed
funkim <mcking172@skiff.com>
2024-02-17 20:03:03 -0500

    Numbers now show up when pressed on the UI

--- calculation.js
+++ calculation.js
@@ -1,9 +1,21 @@
+let savedInputs = [""]
+const output = document.querySelector(".output")
 function showResult() {
-    const correctEquation = parseInt(numberOperator);
-    return parseFloat
+    const displayText = savedInputs.join("")
+    output.innerText = displayText;
+    
+    return output
+
 }
 
 const calculator = document.querySelector(".calc")
+const buttons = document.querySelectorAll("button")
 
+buttons.forEach(button =>{
+button.addEventListener("click", function() {
+savedInputs.push(button.id)
+showResult()
+})
+})
 
 
