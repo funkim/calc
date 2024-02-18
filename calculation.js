2d07973635b917db73707f6a23f5b6ba8b69faee
funkim <mcking172@skiff.com>
2024-02-17 20:47:45 -0500

    Clears the display whenever one of the operators is used and clear works with not just the displayed numbers.

--- calculation.js
+++ calculation.js
@@ -1,21 +1,39 @@
 let savedInputs = [""]
+let displayableInputs = [""]
 const output = document.querySelector(".output")
 function showResult() {
-    const displayText = savedInputs.join("")
+    const displayText = displayableInputs.join("")
     output.innerText = displayText;
-    
     return output
-
 }
 
 const calculator = document.querySelector(".calc")
 const buttons = document.querySelectorAll("button")
+const clear = document.querySelectorAll(".Clear")
+const clearAll = document.querySelector(".ClearAll")
+const enter = document.querySelector(".Enter")
 
+clear.forEach(button =>{
+    button.addEventListener("click", function() {
+displayableInputs = [""]
+return displayableInputs
+    })
+})
 buttons.forEach(button =>{
 button.addEventListener("click", function() {
+    if(["*", "+", "/", "-"].includes(button.id)) {
+displayableInputs = [""]
 savedInputs.push(button.id)
+    } else {
+displayableInputs.push(button.id)
 showResult()
+    }
 })
 })
 
-
+clearAll.addEventListener("click", function(){
+displayableInputs = [""]
+savedInputs = [""]
+return displayableInputs, savedInputs
+}
+)
